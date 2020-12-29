fe7399a4bd2ab1eece8afa10de27a32398e54c1f
Geza Jenes <jenesg@gmail.com>
2020-12-29 19:43:02 +0100

    Commit after part 8

--- accounts/views.py
+++ accounts/views.py
@@ -18,8 +18,15 @@ def home(request):
 
 def products(request):
 	products = Product.objects.all()
-	return render(request, 'accounts/products.html', {'products':products})
 
-def customer(request):
+	context = {'products':products}
+	return render(request, 'accounts/products.html', context)
 
-	return render(request, 'accounts/customer.html')
+def customer(request, pk_test):
+	customer = Customer.objects.get(id=pk_test)
+
+	orders = customer.order_set.all()
+	order_count = orders.count()
+
+	context = {'customer':customer, 'orders':orders, 'order_count':order_count}
+	return render(request, 'accounts/customer.html',context)
